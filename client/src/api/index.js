fc2b06fc3e9de9844b0cdd046b753f0ca9538504
RanjanaSai <ranjana601@gmail.com>
2026-05-31 22:58:21 +0530

    changes in index.js 1

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,10 +1,42 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-export const axiosInstance = axios.create({
-    baseURL = "https://bookmyshow1-342l.onrender.com",
+// export const axiosInstance = axios.create({
+
+//     headers: {
+//         'Content-Type': 'application/json',
+//         'Authorization': `Bearer ${localStorage.getItem('token')}`
+//     }
+// })
+
+import axios from "axios";
+
+const axiosInstance = axios.create({
     headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${localStorage.getItem('token')}`
-    }
-})
+        "Content-Type": "application/json",
+    },
+    baseURL: "/",
+});
 
+// request interceptor
+axiosInstance.interceptors.request.use(
+    function (config) {
+        // Do something before request is sent
+        const token = JSON.parse(localStorage.getItem("auth_token"));
+        if (token) {
+            config.headers["Authorization"] = `Bearer ${token}`
+        }
+        return config;
+    },
+    function (error) {
+        return Promise.reject(error);
+    }
+);
+
+// response interceptor
+// axiosInstance.interceptors.response({
+
+// })
+
+
+
+export default axiosInstance;
