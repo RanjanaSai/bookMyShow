ad877f6224e5ff31107d688ab3a8164691c2bf38
RanjanaSai <ranjana601@gmail.com>
2026-06-02 12:25:45 +0530

    changes emailHelper sendgrid

--- server/utils/emailHelper.js
+++ server/utils/emailHelper.js
@@ -5,17 +5,25 @@ const path = require('path');
 const dotenv = require('dotenv');
 
 dotenv.config();
-// console.log("GMAIL_USER =", process.env.GMAIL_USER);
-// console.log("GMAIL_PASS exists =", !!process.env.GMAIL_PASS);
-const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true, // true for port 465, false for other ports
+
+const { SENDGRID_APIKEY } = process.env;
+// const transporter = nodemailer.createTransport({
+//     host: "smtp.gmail.com",
+//     port: 465,
+//     secure: true, // true for port 465, false for other ports
+//     auth: {
+//         user: process.env.GMAIL_USER,
+//         pass: process.env.GMAIL_PASS,
+//     },
+// });
+const transportDetails = {
+    host: "smtp.sendgrid.net",
+    port: 587,
     auth: {
-        user: process.env.GMAIL_USER,
-        pass: process.env.GMAIL_PASS,
+        user: "apikey",
+        pass: SENDGRID_APIKEY,
     },
-});
+};
 
 const replaceContent = (content, creds) => {
     let allKeysAttr = Object.keys(creds);
