ea9f57dda32376d716e8f04aa04f107c8ca40516
RanjanaSai <ranjana601@gmail.com>
2026-06-02 09:43:02 +0530

    changes index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const axiosInstance = axios.create({
 
-    baseURL: "/",
+    baseURL: " https://bookmyshow1-342l.onrender.com",
     headers: {
         'Content-Type': 'application/json',
         'Authorization': `Bearer ${localStorage.getItem('token')}`
