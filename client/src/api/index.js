a8eb63ea2d4e37bbb383e6e7b20e6082a7b89a25
RanjanaBhanage <114593855+RanjanaSai@users.noreply.github.com>
2026-08-13 22:45:08 +0530

    Fix formatting of baseURL in axios instance

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 export const axiosInstance = axios.create({
 
-    baseURL: " https://bookmyshow1-342l.onrender.com",
+    baseURL: "https://bookmyshow1-342l.onrender.com",
+  
     headers: {
         'Content-Type': 'application/json',
         'Authorization': `Bearer ${localStorage.getItem('token')}`
